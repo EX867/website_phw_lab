98674b117cd5f184c2fe0a2f09cb984a86250388
EX867 <karnosex867@gmail.com>
2025-01-28 23:50:49 +0900

    .

--- src/components/ArticlePreview/ArticlePreview.jsx
+++ src/components/ArticlePreview/ArticlePreview.jsx
@@ -5,16 +5,16 @@ const ArticlePreview = ({article}) => {
 	return (
     <div className="article-preview">
         {article.preview_image === "" ?
-            <img className="size-[8rem] max-w-[40%] object-cover"
+            <img className="w-[10rem] h-[7.5rem] max-w-[40%] object-cover"
                 alt="placeholder image"
                 src="./assets/images/placeholder.svg"/> :
-            <img className="size-[8rem] max-w-[40%] object-cover"
+            <img className="w-[10rem] h-[7.5rem] max-w-[40%] object-cover"
                 alt="preview of article"
                 src={article.preview_image}/>}
         <div>
             <a href={article.src} target="_blank" rel="noreferrer">{article.title} </a>
             <div className="text-text_gray text-smaller py-1">{article.date}</div>
-            <p>{article.preview_content}</p>
+            <div>{article.preview_content}</div>
         </div>
     </div>
   );
--- src/screens/PageHome.jsx
+++ src/screens/PageHome.jsx
@@ -175,6 +175,13 @@ const PageHome = () => {
                                 </div>
                             </div>
                         </div>
+                        <br/><br/>
+                        <iframe width="560" height="315"
+                                src="https://www.youtube-nocookie.com/embed/3ihLLwSEJlI?si=d-aQuNS1EjGAY40o"
+                                title="YouTube video player"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen/>
                     </section>
 
                     <section id="content-news">
