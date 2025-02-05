dd5b1138daa1a783f211b79dfbbff5461c015e61
EX867 <karnosex867@gmail.com>
2025-02-05 17:51:52 +0900

    .

--- src/components/PublicationPreview/PublicationPreview.jsx
+++ src/components/PublicationPreview/PublicationPreview.jsx
@@ -5,8 +5,9 @@ const PublicationPreview = ({article, no_border = true}) => {
 	return (
         <div className="publication-preview" no_border={String(no_border)}>
             <div>{article.author}</div>
-            <div><em>{article.title}</em>
-                {" " + article.journal + " " + article.publication_detail}
+            <div>{article.title + " "}
+                <em>{article.journal + " "}</em>
+                {article.publication_detail}
                 {"doi" in article ? <span>{" doi: "}<a href={"https://doi.org/" + article.doi} target="_blank" rel="noreferrer">{article.doi}</a></span> : ""}
             </div>
             {/* <span dangerouslySetInnerHTML={{__html: article.html_text + " doi: "}}></span> */}
